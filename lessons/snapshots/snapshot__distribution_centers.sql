8e2a3cdac60869f3f9316498f969665c510e3c50
LokeshSharma23 <lsharma@clemson.edu>
2023-06-28 17:35:12 -0400

    created snapshots for distribution center table

---
+++ lessons/snapshots/snapshot__distribution_centers.sql
@@ -0,0 +1,26 @@
+{% snapshot snapshot__distribution_centers  %}
+
+{{
+	config(
+		target_schema= 'dbt_snapshots',
+		unique_key= 'id',
+		strategy= 'check',
+		check_cols= ['name','latitude','longitude']
+	)
+
+
+
+}}
+
+
+SELECT
+	id,
+	name,
+	longitude,
+	latitude
+FROM {{ source('thelook_ecommerce','distribution_centers') }}
+
+
+
+{% endsnapshot %}
+
