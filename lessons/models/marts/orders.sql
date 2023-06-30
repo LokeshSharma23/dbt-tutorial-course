1d92c94ffb5e8b05548f170ed9a287aa6fc585e3
LokeshSharma23 <lsharma@clemson.edu>
2023-06-30 14:26:02 -0400

    loops and setting variables Jinja

--- lessons/models/marts/orders.sql
+++ lessons/models/marts/orders.sql
@@ -8,7 +8,8 @@ order_item_measures AS (
 		SUM(product_cost) AS total_product_cost,
 		SUM(item_profit) AS total_profit,
 		SUM(item_discount) AS total_discount
-		{% set department = ('Men','Women') %}
+		{# This is overkill, but a nice way to show how loops work with dbt Jinja templating #}
+		{%- set departments = ['Men', 'Women'] -%}
 		{%- for department_name in departments %}
 		SUM(IF(product_department = '{{department_name}}', item_sale_price, 0)) AS total_sold_{{department_name.lower()}}swear{% if not loop.last %},{% endif -%}
 		{% endfor %}
