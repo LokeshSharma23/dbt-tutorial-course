ab2760533da5d20b476b459e78bd2627f33e8e2f
LokeshSharma23 <lsharma@clemson.edu>
2023-06-30 14:18:52 -0400

    Hooks and Macros

--- lessons/models/marts/orders.sql
+++ lessons/models/marts/orders.sql
@@ -8,6 +8,10 @@ order_item_measures AS (
 		SUM(product_cost) AS total_product_cost,
 		SUM(item_profit) AS total_profit,
 		SUM(item_discount) AS total_discount
+		{% set department = ('Men','Women') %}
+		{%- for department_name in departments %}
+		SUM(IF(product_department = '{{department_name}}', item_sale_price, 0)) AS total_sold_{{department_name.lower()}}swear{% if not loop.last %},{% endif -%}
+		{% endfor %}
 	FROM {{ ref('int_ecommerce__order_items_products') }}
 	GROUP BY 1
 )
@@ -16,7 +20,9 @@ SELECT
 	-- Dimensions from our Staging Model Table
 	od.order_id,
 	od.created_at AS order_created_at,
+	{{ is_weekend('od.created_at') }} AS order_was_created_on_weekend, -- Macro defined in macros/macro_is_weekend.sql
 	od.shipped_at AS order_shipped_at,
+	{{ is_weekend('od.shipped_at') }} AS order_was_shipped_on_weekend, -- Macro defined in macros/macro_is_weekend.sql
 	od.delivered_at AS order_delivered_at,
 	od.returned_at AS order_returned_at,
 	od.status AS order_status,
